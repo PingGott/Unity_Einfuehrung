35b2b478483fa6ed8d97767c9d3b1f7b733d784b
PingGott <muenzer.d.maximilian1@gmail.com>
2025-02-17 11:13:29 +0100

    End Task 1
    Check Box

--- Assets/Intro/Rotation.cs
+++ Assets/Intro/Rotation.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+
+    public int number;
+    public float speed;
+    public bool rrotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"Die Start Nummer lautet {number}");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (rrotation == true)
+        {
+            float rot = speed * Time.deltaTime;
+            gameObject.transform.Rotate(rot, 4.0f, 3.0f);
+        }
+        else if (rrotation == false)
+        {
+            Debug.Log("Rotation is turned off");
+        }
         
     }
 }
